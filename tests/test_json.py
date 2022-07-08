665eb68490829494ea14b20892d09c4a11202f4c
Jon Connell <python@figsandfudge.com>
2022-07-08 18:24:28 +0100

    Generic test harness

--- tests/test_json.py
+++ tests/test_json.py
@@ -7,16 +7,30 @@ from pathlib import Path
 
 
 def test_json():
-    for filename in Path("tests/data").rglob("*.json"):
-        formatter = Formatter()
-        formatter.indent_spaces = 2
-        formatter.max_inline_length = 120
-
-        ref_filename = str(filename).replace(".json", ".ref")
-        ref = open(ref_filename).read().rstrip()
-
-        with open(filename) as f:
+    test_data_path = Path("tests/data")
+    for source_filename in test_data_path.rglob("*.json"):
+        with open(source_filename) as f:
             obj = json.load(f)
-            json_string = formatter.serialize(obj)
 
-        check.equal(json_string, ref)
+        for ref_filename in test_data_path.rglob(source_filename.stem + ".ref.*"):
+            formatter = Formatter()
+            with open(ref_filename) as f:
+                ref_json = ""
+                for line in f.readlines():
+                    if line.startswith("@"):
+                        (param, value) = line[1:].split("=")
+                        value = value.strip()
+                        param_type = eval(f"type(formatter.{param})")
+                        if param_type == int:
+                            setattr(formatter, param, int(value))
+                        elif param_type == bool:
+                            setattr(formatter, param, bool(value))
+                        else:
+                            setattr(formatter, param, value)
+                    else:
+                        ref_json += line
+            # No final newline
+            ref_json = ref_json.rstrip()
+
+            json_string = formatter.serialize(obj)
+            assert json_string == ref_json
