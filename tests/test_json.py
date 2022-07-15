a8f04d9d9776e4496ed7b86f1c475242ac356449
Jon Connell <python@figsandfudge.com>
2022-07-15 09:57:35 +0100

    Use exec for parameters

--- tests/test_json.py
+++ tests/test_json.py
@@ -12,27 +12,21 @@ def test_json():
     for source_filename in test_data_path.rglob("*.json"):
         if source_filename.match("*.ref*"):
             continue
-        # source_filename = Path("tests/data/test-11.json")
+        # source_filename = Path("tests/data/test-15.json")
         with open(source_filename) as f:
             obj = json.load(f)
 
         for ref_filename in test_data_path.rglob(source_filename.stem + ".ref*"):
             formatter = Formatter()
             print(f"\n*** {ref_filename}")
-            # ref_filename = "tests/data/test-11.ref.3"
+            # ref_filename = "tests/data/test-15.ref.1"
             with open(ref_filename) as f:
                 ref_json = ""
                 for line in f.readlines():
                     if line.startswith("@"):
                         (param, value) = line[1:].split("=")
                         value = value.strip()
-                        param_type = eval(f"type(formatter.{param})")
-                        if param_type == int:
-                            setattr(formatter, param, int(value))
-                        elif param_type == bool:
-                            setattr(formatter, param, bool(value))
-                        else:
-                            setattr(formatter, param, value)
+                        exec(f"formatter.{param} = {value}")
                     else:
                         ref_json += line
             # No final newline
@@ -43,12 +37,12 @@ def test_json():
             if json_string != ref_json:
                 json_string = re.sub("^", ">>", json_string, flags=re.MULTILINE)
                 json_string = re.sub("$", "<<", json_string, flags=re.MULTILINE)
-                ref_json = re.sub("^", ">>", ref_json)
-                ref_json = re.sub("$", "<<", ref_json)
+                ref_json = re.sub("^", ">>", ref_json, flags=re.MULTILINE)
+                ref_json = re.sub("$", "<<", ref_json, flags=re.MULTILINE)
                 print(f"\n=============== {ref_filename} ===============")
                 print("=============== Result ===============")
                 print(json_string)
                 print("\n=============== Reference ===============")
                 print(ref_json)
                 print("==================================\n")
-                # assert json_string == ref_json
+                assert False
