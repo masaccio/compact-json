3fe5ffe6559f555752c3be0fec2eb9c3239ecc27
ZeroRin <pptxp@qq.com>
2023-01-16 12:10:52 +0800

    add test with OrderedDict

--- tests/test_issues.py
+++ tests/test_issues.py
@@ -1,6 +1,4 @@
-import json
-import re
-from pathlib import Path
+from collections import OrderedDict
 
 import pytest
 
@@ -11,7 +9,12 @@ REF_TYPES = """{
   "bool": [true, false], 
   "float": 1.234, 
   "int": [100, 200, 300], 
-  "string": "value"
+  "string": "value", 
+  "ordereddict": {
+    "aaa": 100, 
+    "bbb": 101, 
+    "ccc": 102
+  }
 }"""
 
 
@@ -42,7 +45,8 @@ def test_types():
         "float": 1.234,
         "int": [100, 200, 300],
         "string": "value",
+        "ordereddict": OrderedDict(aaa=100, bbb=101, ccc=102),
     }
-    formatter = Formatter(indent_spaces=2, max_inline_length=80)
+    formatter = Formatter(indent_spaces=2, max_inline_length=30)
     json_string = formatter.serialize(obj)
     assert json_string == REF_TYPES
